d7db2a6f6907160005b4eb5f353326160cff539a
Ajmal <upajmal11@gmail.com>
2024-09-28 23:06:59 +0400

    code updated

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
+# app.py
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Welcome to my Flask REST API!")
-
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify(message="Hello Devloot!")
-
-@app.route('/api/greet/<name>', methods=['GET'])
-def greet(name):
-    return jsonify(message=f"Hello, {name}!")
+    return jsonify(message="Welcome to my Flask Hello World app!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
